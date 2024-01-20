fb6103cfc4fc220c5799b8cb2f333eb0989a7818
irhee1 <156379592+irhee1@users.noreply.github.com>
2024-01-19 20:21:02 -0800

    Update mywebsite.py

--- mywebsite.py
+++ mywebsite.py
@@ -16,8 +16,8 @@ while True:
     num += 1
 latitude = float(latitude)
 longitude = float(longitude)
-latitude -= 8.212
-longitude -= 0.93
+latitude -= 8.21
+longitude -= 0.9
 df = pd.DataFrame({
     "lon":[longitude],
     "lat":[latitude]})
